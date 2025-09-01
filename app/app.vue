d099014e32f8360dda507773e8e8e8c9a6f6f86c
johnhelmuth <john@helmuth.org>
2025-09-01 12:27:25 -0700

    Added link to github repo.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import { version } from '~/../package.json';
+import {version} from '~/../package.json';
 
 const appVersion = `v${version}`;
 const copyrightDate = "2025";
@@ -9,12 +9,15 @@ const copyrightDate = "2025";
 
 <template>
   <div>
-    <NuxtRouteAnnouncer />
+    <NuxtRouteAnnouncer/>
     <h1>Triangle Table Maker</h1>
-    <NuxtPage />
+    <NuxtPage/>
     <footer>
-      <span class="left">Version {{ appVersion }}</span>
+      <span class="left">{{ appVersion }}</span>
       <span class="right">&copy; John Helmuth {{ copyrightDate }}</span>
+      <a class="github-link" href="https://github.com/johnhelmuth/triangle-table-maker" target="_blank">
+        <Icon name="octicon:mark-github-24" class="github-link-icon"/>
+      </a>
     </footer>
   </div>
 </template>
@@ -28,6 +31,18 @@ footer {
   margin: 2rem 1rem 0.5rem;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+}
+footer > * {
+  margin-left: 1rem;
+}
+footer > :first-child {
+  margin-left: 0;
+  margin-right: auto;
+}
+.github-link {
+  font-size: 1.5rem;
+  text-decoration: none;
 }
 </style>
 <script setup lang="ts">
