017f0bd58adb9b96bfdf798c557b3269a4873eb6
johnhelmuth <john@helmuth.org>
2025-09-01 12:03:54 -0700

    Added LICENSE file, footer with copyright notice.

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,21 @@
+<script setup lang="ts">
+
+import { version } from '~/../package.json';
+
+const appVersion = `v${version}`;
+const copyrightDate = "2025";
+
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <h1>Triangle Table Maker</h1>
     <NuxtPage />
+    <footer>
+      <span class="left">Version {{ appVersion }}</span>
+      <span class="right">&copy; John Helmuth {{ copyrightDate }}</span>
+    </footer>
   </div>
 </template>
 
@@ -10,4 +23,12 @@
 h1 {
   text-align: center;
 }
+
+footer {
+  margin: 2rem 1rem 0.5rem;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
+<script setup lang="ts">
+</script>
